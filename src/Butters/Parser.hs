047e07cff6c127adeb1dbba6a861dab6320d826c
Michael Gilliland <mjg.py3@gmail.com>
2015-11-14 00:32:16 -0500

    removed stack-work added some tests

--- src/Butters/Parser.hs
+++ src/Butters/Parser.hs
@@ -1,5 +1,17 @@
 module Butters.Parser where
 
-main :: IO ()
-main = do
-  putStrLn "foobar"
+import Text.ParserCombinators.Parsec
+
+data Constructor =
+  BList [Constructor]
+  deriving (Show, Eq)
+
+data TopLevelDefinition =
+  DataDef String [String] [Constructor]
+  deriving (Show, Eq)
+
+topLevel :: Parser TopLevelDefinition
+topLevel = do
+  return $ DataDef "Zero" [] [BList []]
+
+parseTopLevel = parse topLevel ""
--- test/Butters/TestParser.hs
+++ test/Butters/TestParser.hs
@@ -1,7 +1,17 @@
 module Main where
 
 import Test.QuickCheck
+import Butters.Parser
+
+data ParserTypes =
+  TopLevel String
+
+parsesTo (TopLevel text) = (== (parseTopLevel text)) . Right
+
+test_top_level_data_single_constructor_parses :: Bool
+test_top_level_data_single_constructor_parses =
+  TopLevel "data Zero | []" `parsesTo` DataDef "Zero" [] [BList []]
 
 main :: IO ()
 main =
-  quickCheck True
+  quickCheck test_top_level_data_single_constructor_parses
