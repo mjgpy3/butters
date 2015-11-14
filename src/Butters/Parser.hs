6020cd55e9955ff4e0c4dcf7b61c22a667231a49
Michael Gilliland <mjg.py3@gmail.com>
2015-11-14 00:48:09 -0500

    Next test

--- src/Butters/Parser.hs
+++ src/Butters/Parser.hs
@@ -10,8 +10,28 @@ data TopLevelDefinition =
   DataDef String [String] [Constructor]
   deriving (Show, Eq)
 
+constructorName :: Parser String
+constructorName = do
+  c <- oneOf ['A'..'Z']
+  rest <- many (oneOf $ ['a'..'z'] ++ ['A'..'Z'])
+  return $ c:rest
+
+expression :: Parser Constructor
+expression = do
+  char '['
+  contents <- many expression
+  char ']'
+  return $ BList contents
+
 topLevel :: Parser TopLevelDefinition
 topLevel = do
-  return $ DataDef "Zero" [] [BList []]
+  string "data"
+  char ' '
+  name <- constructorName
+  char ' '
+  char '|'
+  char ' '
+  cons <- expression
+  return $ DataDef name [] [cons]
 
 parseTopLevel = parse topLevel ""
--- test/Butters/TestParser.hs
+++ test/Butters/TestParser.hs
@@ -12,6 +12,14 @@ test_top_level_data_single_constructor_parses :: Bool
 test_top_level_data_single_constructor_parses =
   TopLevel "data Zero | []" `parsesTo` DataDef "Zero" [] [BList []]
 
+prop_arbitrary_depth_single_constructor_parser :: Int -> Property
+prop_arbitrary_depth_single_constructor_parser n =
+  n > 0 ==> (TopLevel $ "data Name | " ++ nestedNTimes) `parsesTo` DataDef "Name" [] expectedBListNesting
+  where
+    nestedNTimes = foldr (\_ o -> "[" ++ o ++ "]") "" [1..n]
+    expectedBListNesting = [foldr (\_ o -> BList [o]) (BList []) [2..n]]
+
 main :: IO ()
-main =
+main = do
   quickCheck test_top_level_data_single_constructor_parses
+  quickCheck prop_arbitrary_depth_single_constructor_parser 
