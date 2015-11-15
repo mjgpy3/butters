5e9f2c4cca7d9a13aadf33febd22a32a28c08360
Michael Gilliland <mjg.py3@gmail.com>
2015-11-15 12:12:55 -0500

    Start TypeChecker

--- src/Butters/TypeChecker.hs
+++ src/Butters/TypeChecker.hs
@@ -1,1 +1,8 @@
 module Butters.TypeChecker where
+
+import Butters.Ast
+
+data TypeCheckingError =
+  RedundantConstructors String Value
+
+checkRedundantConstructors (DataDef _ [] cs) = Left $ RedundantConstructors "" $ BList []
--- test/Butters/TestTypeChecker.hs
+++ test/Butters/TestTypeChecker.hs
@@ -1,5 +1,16 @@
 module TestTypeChecker (testTypeChecker) where
 
+import Butters.Ast
+import Butters.TypeChecker
+import Data.Either
+import Test.QuickCheck
+
+test_redundant_constructors_dont_type_check :: Bool
+test_redundant_constructors_dont_type_check =
+  isLeft $ checkRedundantConstructors $ DataDef "BadNat" [] [BList [], BList []]
+
 testTypeChecker :: IO ()
 testTypeChecker = do
   putStrLn "\n--[TEST] Butters.TypeChecker--\n"
+
+  quickCheck test_redundant_constructors_dont_type_check
